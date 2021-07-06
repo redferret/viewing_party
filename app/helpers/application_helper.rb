9a4aa0de06a38c84b251104b2694f70ab5d25de7
Richard <rdesilvey@gmail.com>
2021-07-06 12:58:39 -0600

    Rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def flash_class(level)
     case level
     when 'notice'
-      return "class='alert alert-info'".html_safe
+      "class='alert alert-info'".html_safe
     when 'success'
-      return "class='alert alert-success'".html_safe
+      "class='alert alert-success'".html_safe
     when 'alert'
-      return "class='alert alert-danger'".html_safe
+      "class='alert alert-danger'".html_safe
     end
   end
 end
