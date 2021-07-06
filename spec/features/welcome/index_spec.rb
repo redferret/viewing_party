12dcf8aa23fb0bf7ec8ae063bc383d79ec1cf2f3
Richard <rdesilvey@gmail.com>
2021-07-06 11:34:41 -0600

    Write tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,10 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Page' do
-  it 'has link to admin dashboard' do
-      visit '/'
+  before :each do
+    visit '/'
+  end
 
-      expect(page).to have_content("Welcome to Viewing Party")
-      expect(page).to have_link("New to Viewing Party? Register Here")
+  describe 'link,' do
+    context 'register,' do
+      it 'nagivates user to a register page' do
+        expect(page).to have_link('New to Viewing Party? Register Here', href: new_register_path)
+
+        click_link 'New to Viewing Party? Register Here'
+
+        expect(current_path).to eq new_register_path
+      end
+    end
+
+    context 'login,' do
+      xit 'logs a user in' do
+        within '#login-form' do
+          fill_in 'user[email]', with: 'test@test.com'
+          fill_in 'user[password]', with: 'password'
+          click_link 'Login'
+        end
+
+        expect(page).to eq dashboard_path
+      end
+    end
+  end
+
+  describe 'login form,' do
+    it 'has an email and password fields' do
+      within '#login-form' do
+        expect(page).to have_field 'user[email]'
+        expect(page).to have_field 'user[password]'
+      end
+    end
   end
 end
