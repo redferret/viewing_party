9d081a33870d598cbfe52414b93baf5095429447
Regina Casias <reginaannvigil@gmail.com>
2021-07-06 16:15:50 -0600

    Created friendship migration model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
 
   has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
   end
+
+  describe "relationships" do
+    it {should have_many :friendships}
+    it {should have_many(:friends).through(:friendships)}
+  end
 end
