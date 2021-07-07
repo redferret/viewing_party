08cbb090507e6ac2345eb31a538d6be81e5c61f5
Richard <rdesilvey@gmail.com>
2021-07-06 19:10:10 -0600

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   scope module: :users do
     resource :dashboard, only: :show
+    resource :discover, controller: :movies, only: :show
+    post '/movies', to: 'movies#show'
     resource :register, controller: :users, only: [:new, :create]
     resource :login, controller: :sessions, only: :create
     resource :logout, controller: :sessions, only: :destroy
