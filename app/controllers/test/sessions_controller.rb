9e32319b542a8292f553bb4926ff0e5bcee0fce5
Richard <rdesilvey@gmail.com>
2021-07-07 12:22:35 -0600

    Test controller not needed

--- app/controllers/test/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Test::SessionsController < ApplicationController
-  def create
-    vars = params.permit(session_vars: {})
-    vars[:session_vars].each do |var, value|
-      session[var] = value
-    end
-    head :created
-  end
-end
