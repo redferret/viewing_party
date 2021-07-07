44d450ccf915a923e692659787202914e3cefca5
Richard <rdesilvey@gmail.com>
2021-07-06 22:20:42 -0600

    Implement API calls

--- app/services/movies_api/api_calls.rb
+++ app/services/movies_api/api_calls.rb
@@ -1,2 +1,16 @@
 module ApiCalls
+  def movie_genres
+    get '/genre/movie/list'
+  end
+
+  def top_rated_movies
+    response = get '/movie/top_rated'
+    response['results']
+  end
+
+  def search_by_title(title)
+    get '/search/movie', params: {
+      query: title
+    }
+  end
 end
