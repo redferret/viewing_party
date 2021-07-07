e7a12192d7190d26ddec5498c5a61655abb06141
Richard <rdesilvey@gmail.com>
2021-07-06 19:14:02 -0600

    Controller notices

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,11 +1,11 @@
 class Users::MoviesController < ApplicationController
   def show
     if params[:search_by_rating].present?
-      flash[:info] = 'Search by Rating'
+      flash[:notice] = 'Search by Rating'
     elsif params[:movie_title].present?
-      flash[:info] = 'Search by title'
+      flash[:notice] = 'Search by title'
     else
-
+      flash[:notice] = 'Discover Movies to Watch!'
     end
   end
 end
