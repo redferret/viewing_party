523546f378a3b6f8218608b6237e8d7719b719eb
Richard <rdesilvey@gmail.com>
2021-07-07 09:17:42 -0600

    Refactor folder name

--- spec/support/fixtures/movies_api_mock.rb
+++ spec/support/fixtures/movies_api_mock.rb
@@ -1,5 +1,5 @@
 class MoviesAPIMock
   def self.get(file_name)
-    File.open(File.dirname(__FILE__) + '/json/' + file_name, 'rb').read
+    File.open(File.dirname(__FILE__) + '/mock_responses/' + file_name, 'rb').read
   end
 end
