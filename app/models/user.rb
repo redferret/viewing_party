6f467c921d07415b5df13dae89c6099fa620fec5
Richard <rdesilvey@gmail.com>
2021-07-07 09:27:44 -0600

    Auto correct w/rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: true
 
   has_secure_password
 end
