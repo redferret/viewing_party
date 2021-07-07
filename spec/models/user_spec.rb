5378cd07034ea1998bb206d1f5cd267811e97ced
Regina Casias <reginaannvigil@gmail.com>
2021-07-06 23:06:01 -0600

    added testing to user model add_friend

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe User do
     it {should have_many :friendships}
     it {should have_many(:friends).through(:friendships)}
   end
+
+  it 'cand add a friend to a user' do
+    user_1 = User.create!(email: "user1@email.com", password_digest: "1234")
+    user_2 = User.create!(email: "user2@email.com", password_digest: "1234")
+    user_3 = User.create!(email: "user3@email.com", password_digest: "1234")
+    user_1.add_friend(user_2)
+    result = Friendship.all.first
+    expect(result.user_id).to eq(user_1.id)
+    expect(result.friend_id).to eq(user_2.id)
+    expect(Friendship.all.count).to eq(1)
+  end
 end
