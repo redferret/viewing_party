d4ac512fe3997c45ae05237d2a10c3afa964d87b
Richard <rdesilvey@gmail.com>
2021-07-06 23:30:44 -0600

    Write feature test

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -8,16 +8,27 @@ RSpec.describe 'Discover movies page' do
     visit discover_path
   end
 
-  describe 'searching for a movie button and form' do
-    it 'has a field to search movies by' do
+  describe 'searching for top rated movies' do
+    it 'has a field to search for top movies' do
       expect(page).to have_link 'Find Top Rated Movies'
     end
+  end
 
-    it 'has a button to find top rated movies' do
+  describe 'searching for movies by title' do
+    it 'has a button to find movies by search query' do
       within 'form' do
         expect(page).to have_field 'movie_title'
         expect(page).to have_button 'Find Movies'
       end
+    end
+
+    it 'returns list of movies from an API call' do
+      within 'form' do
+        fill_in 'movie_title', with: 'Movie Title'
+        click_button 'Find Movies'
+      end
+
+      expect(current_path).to eq movies_path
     end
   end
 end
