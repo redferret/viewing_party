f7a41e8a65c37134a7dfee3da6b71429013cb6ea
Richard <rdesilvey@gmail.com>
2021-07-09 17:30:32 -0600

    Write feature tests

--- spec/features/users/movie_parties/new_spec.rb
+++ spec/features/users/movie_parties/new_spec.rb
@@ -10,19 +10,37 @@ RSpec.describe 'Creating a movie party page' do
     @main_user.friends << @friend_1 << @friend_2 << @friend_3
 
     login_with @main_user
+
+    visit new_movie_party_path(movie_id: 0)
   end
 
   describe 'movie details form' do
-    it 'has a title'
-    it 'has the movie title'
-    it 'has duration of the party field'
-    it 'has date of the party field'
-    it 'has time to watch field'
+    it 'has a title' do
+      save_and_open_page
+      expect(page).to have_content('Movie Party Details')
+    end
+
+    it 'has all the fields to make a movie party' do
+      within '#movie-details' do
+        expect(page).to have_field('movie_title', with: 'Fight Club')
+        expect(page).to have_field('duration_of_party')
+        expect(page).to have_field('duration_of_party')
+        expect(page).to have_field('time_of_viewing')
+        expect(page).to have_button('Create Party')
+      end
+    end
   end
 
   describe 'friends list in form' do
-    it 'has the list of friends to add to the party'
-    it 'has a checkbox near each friend'
+    it 'has the list of friends to add to the party' do
+      within '#friends-list' do
+        expect(page).to have_content(@friend_1.email)
+        expect(page).to have_content(@friend_2.email)
+        expect(page).to have_content(@friend_3.email)
+      end
+      within "#friend-#{@friend_1.id}" do
+        expect(page).to have_field('', type: 'checkbox')
+      end
+    end
   end
-  
 end
