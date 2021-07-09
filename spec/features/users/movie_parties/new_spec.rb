ad18d5f868ab31f140e3b7f412d7e0d79003029d
Richard <rdesilvey@gmail.com>
2021-07-08 21:08:02 -0600

    Setup feature tests

---
+++ spec/features/users/movie_parties/new_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Creating a movie party page' do
+  describe 'movie details form' do
+    it 'has a title'
+    it 'has the movie title'
+    it 'has duration of the party field'
+    it 'has date of the party field'
+    it 'has time to watch field'
+  end
+
+  describe 'friends list in form' do
+    it 'has the list of friends to add to the party'
+    it 'has a checkbox near each friend'
+  end
+  
+end
