aacba68ba7152ce961461cae0ff89808385dbfd1
Richard <rdesilvey@gmail.com>
2021-07-10 15:24:22 -0600

    Add model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,22 +12,29 @@ RSpec.describe User do
     it {should have_many(:friends).through(:friendships)}
   end
 
-  it 'cand add a friend to a user' do
-    user_1 = FactoryBot.create(:user, email: "user1@email.com")
-    user_2 = FactoryBot.create(:user, email: "user2@email.com")
-    user_3 = FactoryBot.create(:user, email: "user3@email.com")
-    user_1.add_friend(user_2)
-    result = Friendship.all.first
+  describe 'model method,' do
+    context '#add_friend' do
+      it 'adds a friend to this user' do
+        user_1 = FactoryBot.create(:user, email: "user1@email.com")
+        user_2 = FactoryBot.create(:user, email: "user2@email.com")
+    
+        user_1.add_friend(user_2)
+        result = Friendship.first
 
-    expect(result.user_id).to eq(user_1.id)
-    expect(result.friend_id).to eq(user_2.id)
-    expect(Friendship.all.count).to eq(1)
+        expect(user_1.friends.include?(user_2)).to eq true
+      end
+    end
+    
+    context '#remove_friend' do
+      it 'removes a friend from this user' do
+        user_1 = FactoryBot.create(:user, email: "user1@email.com")
+        user_2 = FactoryBot.create(:user, email: "user2@email.com")
+    
+        user_1.add_friend(user_2)
+        user_1.remove_friend(user_2)
 
-    user_1.add_friend(user_3)
-    result2 = Friendship.all.last
-
-    expect(result2.user_id).to eq(user_1.id)
-    expect(result2.friend_id).to eq(user_3.id)
-    expect(Friendship.all.count).to eq(2)
+        expect(user_1.friends.include?(user_2)).to eq false
+      end
+    end
   end
 end
