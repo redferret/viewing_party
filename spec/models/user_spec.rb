1a0e40b4e7ee335ae74eef2ab2e9e61846455b86
Richard <rdesilvey@gmail.com>
2021-07-10 22:58:13 -0600

    write more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe User do
   end
 
   describe "relationships" do
-    it {should have_many :friendships}
-    it {should have_many :movie_parties}
-    it {should have_many(:friends).through(:friendships)}
+    it { should have_many :friendships }
+    it { should have_many :movie_parties }
+    it { should have_many(:friends).through(:friendships) }
   end
 
   describe 'model method,' do
@@ -30,10 +30,31 @@ RSpec.describe User do
         user_1 = FactoryBot.create(:user, email: "user1@email.com")
         user_2 = FactoryBot.create(:user, email: "user2@email.com")
     
-        user_1.add_friend(user_2)
+        user_1.friends << user_2
         user_1.remove_friend(user_2)
 
         expect(user_1.friends.include?(user_2)).to eq false
+      end
+      
+      it 'removes any invitations after removing a friend who invited you' do
+        user_1 = FactoryBot.create(:user, email: "user1@email.com")
+        user_2 = FactoryBot.create(:user, email: "user2@email.com")
+
+        user_2.friends << user_1
+        user_1.friends << user_2
+
+        movie_party = FactoryBot.create(:movie_party, user: user_2)
+
+        friendship = user_2.friendships.find_by(friend_id: user_1.id)
+        Invitation.create(movie_party_id: movie_party.id, friendship_id: friendship.id)
+
+        user_1.remove_friend(user_2)
+
+        expect(user_1.friends.include?(user_2)).to eq false
+
+        invitations = Invitation.find_invitations(user_1.id)
+
+        expect(invitations.empty?).to eq true
       end
     end
   end
