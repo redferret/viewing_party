a527a91ab953865fa5fe41c35d7251515e76277a
Richard <rdesilvey@gmail.com>
2023-08-09 11:35:12 -0600

    Update services to use VCR

--- spec/facades/index_facade_spec.rb
+++ spec/facades/index_facade_spec.rb
@@ -1,21 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Movies::IndexFacade do
-  before :each do
-    trending_movies_mock_path = EndpointStitch::stitch(MoviesAPI::Client::trending_movies_endpoint)
-    trending_movies_mock_data = MoviesAPIMock::get('trending_movies.json')
-    stub_request(:get, trending_movies_mock_path)
-      .with(headers: test_headers).to_return(status: 200, body: trending_movies_mock_data, headers: {})
-  end
+  describe 'trending_movies' do
+    subject { Movies::IndexFacade.trending_movies }
 
-  describe 'method' do
-    context '::trending_movies' do
-      it 'returns an array of Poros::Movies' do
-        movies = Movies::IndexFacade.trending_movies
-        
-        expect(movies).to be_an Array
-        expect(movies.first).to be_a Poros::Movie
-      end
+    it 'returns an array of Poros::Movies', :vcr do
+      response = subject
+
+      expect(response).to be_an Array
+      expect(response.first).to be_a Poros::Movie
     end
   end
 end
