548152890e85492b6e9abeff26bdbf0c1e78b8c7
Richard <rdesilvey@gmail.com>
2023-09-26 12:47:45 -0600

    Other updates

--- spec/services/movies_api/api_calls_spec.rb
+++ spec/services/movies_api/api_calls_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe MoviesAPI::Client do
         movies = MoviesAPI::Client.trending_movies
 
         expect(movies).to be_an Array
-        expect(movies.first[:title]).to eq 'Spider-Man: Across the Spider-Verse'
-        expect(movies.first[:vote_average]).to eq 8.5
+        expect(movies.first[:title]).to be_a String
+        expect(movies.first[:vote_average]).to be_a Float
       end
     end
 
@@ -17,8 +17,8 @@ RSpec.describe MoviesAPI::Client do
         movies = MoviesAPI::Client.top_rated_movies
 
         expect(movies).to be_an Array
-        expect(movies.first[:title]).to eq 'The Godfather'
-        expect(movies.first[:vote_average]).to eq 8.7
+        expect(movies.first[:title]).to be_a String
+        expect(movies.first[:vote_average]).to be_a Float
       end
     end
 
@@ -27,8 +27,8 @@ RSpec.describe MoviesAPI::Client do
         movies = MoviesAPI::Client.search_by_title('Movie Title')
 
         expect(movies).to be_an Array
-        expect(movies.first[:title]).to eq 'Not Another B Movie'
-        expect(movies.first[:vote_average]).to eq 5.8
+        expect(movies.first[:title]).to be_a String
+        expect(movies.first[:vote_average]).to be_a Float
       end
     end
 
@@ -37,16 +37,16 @@ RSpec.describe MoviesAPI::Client do
         movies = MoviesAPI::Client.movie_details(550)
 
         expect(movies).to be_a Hash
-        expect(movies[:title]).to eq("Fight Club")
-        expect(movies[:id]).to eq(550)
+        expect(movies[:title]).to be_a String
+        expect(movies[:id]).to be_an Integer
         expect(movies[:credits]).to be_a Hash
         expect(movies[:credits][:cast]).to be_a Array
-        expect(movies[:credits][:cast].first[:name]).to eq("Edward Norton")
-        expect(movies[:credits][:cast].first[:character]).to eq("The Narrator")
-        expect(movies[:credits][:cast].second[:name]).to eq("Brad Pitt")
-        expect(movies[:credits][:cast].second[:character]).to eq("Tyler Durden")
+        expect(movies[:credits][:cast].first[:name]).to be_a String
+        expect(movies[:credits][:cast].first[:character]).to be_a String
+        expect(movies[:credits][:cast].second[:name]).to be_a String
+        expect(movies[:credits][:cast].second[:character]).to be_a String
         expect(movies[:reviews][:results]).to be_an Array
-        expect(movies[:reviews][:results].first[:author]).to eq("Goddard")
+        expect(movies[:reviews][:results].first[:author]).to be_a String
         expect(movies[:reviews][:results].first[:content]).to be_a String
       end
     end
@@ -56,9 +56,9 @@ RSpec.describe MoviesAPI::Client do
         movies = MoviesAPI::Client.upcoming_movies
 
         expect(movies).to be_an Array
-        expect(movies.first[:poster_path]).to eq("/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg")
-        expect(movies.first[:release_date]).to eq("2023-07-19")
-        expect(movies.first[:title]).to eq("Barbie")
+        expect(movies.first[:poster_path]).to eq("/kdPMUMJzyYAc4roD52qavX0nLIC.jpg")
+        expect(movies.first[:release_date]).to eq("2023-07-26")
+        expect(movies.first[:title]).to eq("Talk to Me")
       end
     end
   end
